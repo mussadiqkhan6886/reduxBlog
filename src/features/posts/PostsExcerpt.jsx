907554ca480c5b54095d7f988c81f51610e0e8d5
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-07-13 21:42:24 +0500

    completed adding styling

--- src/features/posts/PostsExcerpt.jsx
+++ src/features/posts/PostsExcerpt.jsx
@@ -2,9 +2,13 @@ import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
 import { Link } from 'react-router-dom';
-import React from "react";
 
-let PostsExcerpt = ({ post }) => {
+import { useSelector } from "react-redux";
+import { selectPostById } from "./postsSlice";
+
+const PostsExcerpt = ({ postId }) => {
+    const post = useSelector(state => selectPostById(state, postId))
+
     return (
         <article>
             <h2>{post.title}</h2>
@@ -18,5 +22,5 @@ let PostsExcerpt = ({ post }) => {
         </article>
     )
 }
-PostsExcerpt = React.memo(PostsExcerpt)
+
 export default PostsExcerpt
