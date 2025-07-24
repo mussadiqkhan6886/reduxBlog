8db284e4a4420572a2110c12ce12742359381916
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-07-24 19:19:36 +0500

    used rtk query

--- src/features/users/UserPage.jsx
+++ src/features/users/UserPage.jsx
@@ -1,27 +1,41 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { selectUserById } from '../users/usersSlice'
 import { Link, useParams } from 'react-router-dom'
-import { selectUserById } from './usersSlice'
-import { selectAllPosts, selectPostsByUser } from '../posts/postsSlice'
+import { useGetPostsByUserIdQuery } from '../posts/postsSlice'
 
 const UserPage = () => {
-    const {userId} = useParams()
+    const { userId } = useParams()
     const user = useSelector(state => selectUserById(state, Number(userId)))
 
-    const postForUsers = useSelector(state => selectPostsByUser(state, Number(userId)))
+    const {
+        data: postsForUser,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetPostsByUserIdQuery(userId);
 
-    const postTitles = postForUsers.map(post => (
-        <li className='mb-2 hover:underline hover:text-white' key={post.id}>
-             <Link to={`/post/${post.id}`}>{post.title.charAt(0).toUpperCase() + post.title.slice(1)}</Link>
-        </li>
-    ))
+    let content;
+    if (isLoading) {
+        content = <p>Loading...</p>
+    } else if (isSuccess) {
+        const { ids, entities } = postsForUser
+        content = ids.map(id => (
+            <li key={id}>
+                <Link to={`/post/${id}`}>{entities[id].title}</Link>
+            </li>
+        ))
+    } else if (isError) {
+        content = <p>{error}</p>;
+    }
 
-  return (
-    <section className="h-[581px] bg-gradient-to-b from-gray-800 to-purple-900 px-12 flex flex-col items-center">
+    return (
+       <section className="h-[581px] bg-gradient-to-b from-gray-800 to-purple-900 px-12 flex flex-col items-center">
       <h2 className='text-3xl font-bold text-white my-2'>{user?.name}</h2>
-      <ol className='font-semibold text-xl text-gray-300'>{postTitles}</ol>
+      <ol className='font-semibold text-xl text-gray-300'>{content}</ol>
     </section>
-  )
+    )
 }
 
 export default UserPage
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,25 @@
-import React, { StrictMode } from 'react';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import { fetchPosts } from './features/posts/postsSlice';
-import { fetchUsers } from './features/users/usersSlice';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { store } from './app/store.js'
+import { Provider } from 'react-redux'
+import { extendedApiSlice } from './features/posts/postsSlice.js'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { usersApiSlice } from './features/users/usersSlice.js'
 
-store.dispatch(fetchPosts());
-store.dispatch(fetchUsers());
+
+store.dispatch(extendedApiSlice.endpoints.getPosts.initiate());
+store.dispatch(usersApiSlice.endpoints.getUsers.initiate());
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
+   <Provider store={store}>
+    <Router> 
+      <Routes>
+      <Route path='/*' element={<App />} />
+      </Routes>
       </Router>
-    </Provider>
+   </Provider>
   </StrictMode>,
 )
